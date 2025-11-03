c365e0dab681d4752dfa0849a6e0f3fc0bf44548
andile96 <innocentandile528@gmail.com>
2025-11-03 02:22:40 +0200

    CollectMethodImplementation

---
+++ src/main/java/NumberRangeSummarizer/NumberRangeSummarizer.java
@@ -0,0 +1,43 @@
+package NumberRangeSummarizer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class NumberRangeSummarizer implements INumberRangeSummarizer
+{
+    @Override
+    public Collection<Integer> collect(String input) {
+        if((input == null) || input.trim().isEmpty())
+        {
+            return new ArrayList<>();
+        }
+
+        try {
+            String[] split = input.split(",");
+            List<Integer> num = new ArrayList<>();
+
+
+            for (String s : split)
+            {
+                num.add(Integer.parseInt(s.trim()));
+            }
+
+            Collections.sort(num);
+
+            return num.stream().distinct().toList();
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("Invalid input. Try again, Only numbers and commas are allowed!.");
+            return Collections.emptyList();
+
+        }
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        return "";
+    }
+}
